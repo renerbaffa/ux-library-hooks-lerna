938417de095b7ab26ab11562de199029d426eb98
Rener Baffa da Silva <renerbaffa@gmail.com>
2020-03-26 21:54:12 +0100

    adding main package created with CRA

---
+++ packages/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from 'button'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>frontend</div>
+        <Button>Button</Button>
+      </header>
+    </div>
+  )
+}
+
+export default App;
